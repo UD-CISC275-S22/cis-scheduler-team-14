8074cffe918327f4539fedb60ca112a7f5576a59
sonika <sonika@udel.edu>
2022-04-05 15:17:06 -0400

    Added my name

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App(): JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+            <p>Sonika!</p>
         </div>
     );
 }
