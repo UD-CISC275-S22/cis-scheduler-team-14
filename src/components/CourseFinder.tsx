0a7e0a517197d3f5be41d6dbfba95b803c8bb56f
John Bean <johnbean@wifi-roaming-128-4-68-178.wifi.udel.edu>
2022-04-07 19:02:39 -0400

    @jbeans0312 Added radio buttons for selecting search fields

--- src/components/CourseFinder.tsx
+++ src/components/CourseFinder.tsx
@@ -1,8 +1,26 @@
 import { useState } from "react";
+import { Course, CreditType } from "../interfaces/course";
+import { SemesterSeason } from "../interfaces/semester";
 
 /** A search tab to search for a certain course within the given course catalog */
-export function CourseFinder(): JSX.Element {
+export function CourseFinder(courseData: Course[]): JSX.Element {
     const [query, setQuery] = useState<string>("");
     const [queryType, setQueryType] = useState<string>("id");
-    const [results, setResults] = useState<Course[]>();
+
+    /** Imports an array of Course objects, type casts creditTypes and semestersOffered */
+    const COURSES = courseData.map(
+        (course): Course => ({
+            ...course,
+            creditTypes: course.creditTypes as CreditType[],
+            semestersOffered: course.semestersOffered as SemesterSeason[]
+        })
+    );
+    const [results, setResults] = useState<Course[]>(COURSES);
+
+    /** A row of radio buttons for selecting what to search for [ID, NAME, SEMESTERS OFFERED, CREDIT TYPES] */
+    function SearchTypeButtons();
+
+    return(
+        
+    );
 }
