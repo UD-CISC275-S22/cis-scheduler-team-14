5eb41b3b3b27fe3c1a2e1bc04f17a4a512219acc
John Bean <johnbean@wifi-roaming-128-4-68-178.wifi.udel.edu>
2022-04-07 19:02:50 -0400

    @jbeans0312 Added radio buttons for selecting search fields

--- src/components/CourseFinder.tsx
+++ src/components/CourseFinder.tsx
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import React, { ReactHTML, useState } from "react";
+import { Form } from "react-bootstrap";
 import { Course, CreditType } from "../interfaces/course";
 import { SemesterSeason } from "../interfaces/semester";
 
 /** A search tab to search for a certain course within the given course catalog */
 export function CourseFinder(courseData: Course[]): JSX.Element {
     const [query, setQuery] = useState<string>("");
-    const [queryType, setQueryType] = useState<string>("id");
+    const [field, setField] = useState<string>("ID");
 
     /** Imports an array of Course objects, type casts creditTypes and semestersOffered */
     const COURSES = courseData.map(
@@ -18,9 +19,34 @@ export function CourseFinder(courseData: Course[]): JSX.Element {
     const [results, setResults] = useState<Course[]>(COURSES);
 
     /** A row of radio buttons for selecting what to search for [ID, NAME, SEMESTERS OFFERED, CREDIT TYPES] */
-    function SearchTypeButtons();
+    function SearchTypeButtons(): JSX.Element {
+        const SEARCH_FIELDS = [
+            "ID",
+            "Name",
+            "Semesters Offered",
+            "Credit Types"
+        ];
+        function updateField(event: React.ChangeEvent<HTMLInputElement>) {
+            setField(event.target.value);
+        }
+        return (
+            <div>
+                {SEARCH_FIELDS.map((FIELD: string) => (
+                    <Form.Check
+                        inline
+                        key={FIELD}
+                        type="radio"
+                        name="field"
+                        id="radio-button-fields"
+                        label={FIELD}
+                        value={FIELD}
+                        checked={FIELD === field}
+                        onChange={updateField}
+                    />
+                ))}
+            </div>
+        );
+    }
 
-    return(
-        
-    );
+    return <div>Course Finder</div>;
 }
