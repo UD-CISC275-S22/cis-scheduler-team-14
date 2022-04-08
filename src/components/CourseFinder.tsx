0996b42b20ab33ba3f5b438c0536a216cfa8212d
John Bean <johnbean@wifi-roaming-128-4-68-178.wifi.udel.edu>
2022-04-08 12:16:45 -0400

    @jbeans0312 More work on CourseFinder.tsx, added function to filter through course results based on query.

--- src/components/CourseFinder.tsx
+++ src/components/CourseFinder.tsx
@@ -1,6 +1,6 @@
 import React, { ReactHTML, useState } from "react";
 import { Form } from "react-bootstrap";
-import { Course, CreditType } from "../interfaces/course";
+import { Course, CreditType, getCourseString } from "../interfaces/course";
 import { SemesterSeason } from "../interfaces/semester";
 
 /** A search tab to search for a certain course within the given course catalog */
@@ -15,8 +15,14 @@ export function CourseFinder(courseData: Course[]): JSX.Element {
             semestersOffered: course.semestersOffered as SemesterSeason[]
         })
     );
-    const [results, setResults] = useState<Course[]>(COURSES);
+    
+    /** Maps each of the Course objects in COURSE to strings to be displayed in the search window */
+    const COURSE_STRINGS = COURSES.map(
+        (course: Course): string => getCourseString(course)
+    );
 
+    const [results, setResults] = useState<Course[]>(COURSES);
+    
     /** A text field for entering search terms */
     function SearchTextField(): JSX.Element {
         function updateQuery(event: React.ChangeEvent<HTMLInputElement>) {
@@ -25,9 +31,37 @@ export function CourseFinder(courseData: Course[]): JSX.Element {
         return (
             <Form.Group controlId="formCourseSearch">
                 <Form.Label>Course lookup:</Form.Label>
-                <Form.Control value={query} onChange={updateQuery} />
+                <Form.Control
+                    placeholder="Enter class name"
+                    value={query}
+                    onChange={updateQuery}
+                />
             </Form.Group>
         );
     }
-    return <div>Course Finder</div>;
+
+    /** A function to filter through courses given a query */
+    function updateResults(){
+        if(query === ""){
+            setResults(COURSES.map(
+                (course: Course) => ({
+                    ...course,
+                    creditTypes: course.creditTypes as CreditType[],
+                    semestersOffered: course.semestersOffered as SemesterSeason[]
+                })
+            ));
+        }else{
+            const containsQuery = ((course: Course): boolean => getCourseString(course).toLowerCase().indexOf(query.toLowerCase()) !== -1);
+            
+        }
+    }
+
+    return(
+        <div>
+            <SearchTextField></SearchTextField>
+            {
+                COURSE_STRINGS.filter(course: string): boolean =>
+            }
+        </div>
+    );
 }
