fc972fb31e0a7c4b3aec2a6137c82c989fb9e3da
John Bean <johnbean@wifi-roaming-128-4-68-178.wifi.udel.edu>
2022-04-07 20:53:14 -0400

    @jbeans0312 Realized that the radio buttons were reduntant at the moment, put the code in a paste bin for now.

--- src/components/CourseFinder.tsx
+++ src/components/CourseFinder.tsx
@@ -6,7 +6,6 @@ import { SemesterSeason } from "../interfaces/semester";
 /** A search tab to search for a certain course within the given course catalog */
 export function CourseFinder(courseData: Course[]): JSX.Element {
     const [query, setQuery] = useState<string>("");
-    const [field, setField] = useState<string>("ID");
 
     /** Imports an array of Course objects, type casts creditTypes and semestersOffered */
     const COURSES = courseData.map(
@@ -18,35 +17,17 @@ export function CourseFinder(courseData: Course[]): JSX.Element {
     );
     const [results, setResults] = useState<Course[]>(COURSES);
 
-    /** A row of radio buttons for selecting what to search for [ID, NAME, SEMESTERS OFFERED, CREDIT TYPES] */
-    function SearchTypeButtons(): JSX.Element {
-        const SEARCH_FIELDS = [
-            "ID",
-            "Name",
-            "Semesters Offered",
-            "Credit Types"
-        ];
-        function updateField(event: React.ChangeEvent<HTMLInputElement>) {
-            setField(event.target.value);
+    /** A text field for entering search terms */
+    function SearchTextField(): JSX.Element {
+        function updateQuery(event: React.ChangeEvent<HTMLInputElement>) {
+            setQuery(event.target.value);
         }
         return (
-            <div>
-                {SEARCH_FIELDS.map((FIELD: string) => (
-                    <Form.Check
-                        inline
-                        key={FIELD}
-                        type="radio"
-                        name="field"
-                        id="radio-button-fields"
-                        label={FIELD}
-                        value={FIELD}
-                        checked={FIELD === field}
-                        onChange={updateField}
-                    />
-                ))}
-            </div>
+            <Form.Group controlId="formCourseSearch">
+                <Form.Label>Course lookup:</Form.Label>
+                <Form.Control value={query} onChange={updateQuery} />
+            </Form.Group>
         );
     }
-
     return <div>Course Finder</div>;
 }
