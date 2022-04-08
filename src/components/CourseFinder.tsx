49b981265d13cd18ea0a1020da1c693f749d8491
John Bean <johnbean@wifi-roaming-128-4-68-178.wifi.udel.edu>
2022-04-08 13:37:32 -0400

    @jbeans0312 First iteration of CourseFinder.tsx (UNTESTED)

--- src/components/CourseFinder.tsx
+++ src/components/CourseFinder.tsx
@@ -1,4 +1,4 @@
-import React, { ReactHTML, useState } from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import { Course, CreditType, getCourseString } from "../interfaces/course";
 import { SemesterSeason } from "../interfaces/semester";
@@ -9,24 +9,24 @@ export function CourseFinder(courseData: Course[]): JSX.Element {
 
     /** Imports an array of Course objects, type casts creditTypes and semestersOffered */
     const COURSES = courseData.map(
-        (course): Course => ({
+        (course: Course): Course => ({
             ...course,
             creditTypes: course.creditTypes as CreditType[],
             semestersOffered: course.semestersOffered as SemesterSeason[]
         })
     );
-    
-    /** Maps each of the Course objects in COURSE to strings to be displayed in the search window */
-    const COURSE_STRINGS = COURSES.map(
-        (course: Course): string => getCourseString(course)
+
+    /** Creates an array of Course strings using the getCourseString function in course.ts */
+    const COURSE_STRINGS = COURSES.map((course: Course): string =>
+        getCourseString(course)
     );
 
-    const [results, setResults] = useState<Course[]>(COURSES);
-    
+    const [results, setResults] = useState<string[]>(COURSE_STRINGS);
     /** A text field for entering search terms */
     function SearchTextField(): JSX.Element {
         function updateQuery(event: React.ChangeEvent<HTMLInputElement>) {
             setQuery(event.target.value);
+            updateResults();
         }
         return (
             <Form.Group controlId="formCourseSearch">
@@ -40,28 +40,25 @@ export function CourseFinder(courseData: Course[]): JSX.Element {
         );
     }
 
-    /** A function to filter through courses given a query */
-    function updateResults(){
-        if(query === ""){
-            setResults(COURSES.map(
-                (course: Course) => ({
-                    ...course,
-                    creditTypes: course.creditTypes as CreditType[],
-                    semestersOffered: course.semestersOffered as SemesterSeason[]
-                })
-            ));
-        }else{
-            const containsQuery = ((course: Course): boolean => getCourseString(course).toLowerCase().indexOf(query.toLowerCase()) !== -1);
-            
+    /** A function to filter through an array of courses (COURSES by default) given a query and update the results useState*/
+    function updateResults() {
+        if (query === "") {
+            setResults(COURSE_STRINGS);
+        } else {
+            const containsQuery = (course: string): boolean =>
+                course.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+            setResults(COURSE_STRINGS.filter(containsQuery));
         }
     }
 
-    return(
+    return (
         <div>
             <SearchTextField></SearchTextField>
-            {
-                COURSE_STRINGS.filter(course: string): boolean =>
-            }
+            {results.map((courseText: string) => (
+                <div key={courseText}>
+                    <span>{courseText}</span>
+                </div>
+            ))}
         </div>
     );
 }
