f90465381f062e8173106d848a4b4a1b47a9058a
wcgunter <wgunter@udel.edu>
2022-04-24 13:02:25 -0400

    Remove unnecessary files.

--- src/components/Semester.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-import "../interfaces/semester";
-import { Semester } from "../interfaces/semester";
-
-export function clearAllSemester() {
-    const [semester, setSemester] = useState<Semester[]>([]);
-    setSemester([]);
-    semester;
-}
